d2e390659131c507343a28eeb0ffca73ae3ffb11
matiasvian <matiasjv@stud.ntnu.no>
2021-03-19 14:39:51 +0100

    Redirection and forking

--- wish.c
+++ wish.c
@@ -5,137 +5,15 @@
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
-#include <fcntl.h> 
+#include <fcntl.h>
+
+// Uncomment to have the shell print the interpreted input.
+// #define ECHO
 
 // Global variables
 char *tokens[50];
 char input[200];
-
-void io(char type, char path[]) {
-
-    /*  The return value of open() is a file descriptor, a small,
-    *   nonnegative integer that is used in subsequent system calls
-    *   (read(2), write(2), lseek(2), fcntl(2), etc.) to refer to the
-    *   open file.
-    *   must include one of the following access
-    *   modes: O_RDONLY, O_WRONLY, or O_RDWR.
-    *   return value of open() is a file descriptor
-    *    
-    *   dup(2) copies the file descriptor.
-    * 
-    *   dup2(2) is used to redirect standard input (0) standard output (1) or error (2)
-    *   Example: dup2("file descriptor", 1); // Writes what would usually be printed to the file.
-    */
-
-    if (strcmp(type, "<") == 0) {
-        
-        int file_desc = open(path, O_CREAT | O_RDWR);
-        
-        // –––––––––––––––––––– Error handling ––––––––––––––––––––
-        if (file_desc < 0) {
-            printf("Error opening the file\n"); 
-        }
-        if (errno != 0) {
-            perror("open");
-            printf("Error code %i.", errno);
-            errno = 0;
-        }
-
-        // Redirection of output to 
-        dup2(file_desc, 1);
-        close(file_desc);
-
-        if (errno != 0) {
-            perror("dup");
-            printf("Error code %i.", errno);
-            errno = 0;
-        }
-
-        read(file_desc, arguments[i-1], sizeof(arguments[i-1]));
-        
-        // –––––––––––––––––––– Error handling ––––––––––––––––––––
-        if (errno != 0) {
-            perror("read");
-            printf("Error code %i.", errno);
-            errno = 0;
-        }
-    }
-    else if (strcmp(type, ">") == 0) {
-        
-        int file_desc = open(arguments[i+1], O_CREAT | O_RDWR);
-        
-        // –––––––––––––––––––– Error handling ––––––––––––––––––––
-        if (file_desc < 0) {
-            printf("Error opening the file\n"); 
-        }
-        if (errno != 0) {
-            perror("open");
-            printf("Error code %i.", errno);
-            errno = 0;
-        }
-
-        write(file_desc, arguments[i-1], sizeof(arguments[i-1]));
-        
-        // –––––––––––––––––––– Error handling ––––––––––––––––––––
-        if (errno != 0) {
-            perror("write");
-            printf("Error code %i.", errno);
-            errno = 0;
-        }
-    }
-    else if (strcmp(type, ">>") == 0) {
-
-    }
-}
-
-// Splits input into tokens, stores it in the global variable tokens
-// and returns the index of last token + 1.
-int tokenize() {
-    int i = 0;
-    
-    // Get first token
-    char *token = strtok(input, " ");
-
-    // Get the remaining tokens if any
-    while (token != NULL) {
-
-        // Check if there is a redirection
-        if (strcmp(token, "<") == 0 || strcmp(token, ">") == 0) {
-            redirection = *token;
-
-            // Get the redirection path
-            token = strtok(NULL, " ");
-
-            // Printing the type and direction, skip them for the final tokenized file
-            printf("Redirection type: %c\n", redirection);
-            printf("Direction: %s\n", token);
-            
-            io(redirection, token);
-
-            break; 
-        }
-        else {
-        tokens[i++] = token;
-        token = strtok(NULL, " ");
-        }
-    }
-    
-    // This is normally the index of tokens' first NULL,
-    // but if tokens was entirely filled, i will be
-    // equal to the size of tokens (50, that is).
-    return i;
-}
-
-// Prompt the user for input and store it in the global variable input.
-void scanInput() {
-    // Get input
-    printf("$ ");
-    fgets(input, sizeof(input), stdin);
-    
-    // Remove newline character at end of last argument
-    int length = strlen(input);
-    input[length-1] = '\0';
-}
+bool redirection;
 
 // Executes the tokenized command. Fork first
 void execute(int length) {
@@ -157,15 +35,145 @@ void execute(int length) {
     }
 }
 
+void io(char *type, char *path, int length) {
+
+    /*  The return value of open() is a file descriptor, a small,
+    *   nonnegative integer.
+    * 
+    *   dup2(2) is used to redirect standard input (0) standard output (1) or error (2)
+    *   Example: dup2("file descriptor", 1); // Writes what would usually be printed to the file.
+    */
+
+    if (strcmp(type, ">") == 0) {
+        
+        // Open file
+        int file_desc = open(path, O_CREAT | O_RDWR);
+        
+        // –––––––––––––––––––– Error handling ––––––––––––––––––––
+        if (file_desc < 0) {
+            printf("Error opening the file\n"); 
+        }
+        if (errno != 0) {
+            perror("open");
+            printf("Error code %i.", errno);
+            errno = 0;
+        }
+
+        // Redirection of output to file
+        dup2(file_desc, 1);
+        close(file_desc);
+
+        // –––––––––––––––––––– Error handling ––––––––––––––––––––
+        if (errno != 0) {
+            perror("dup");
+            printf("Error code %i.", errno);
+            errno = 0;
+        }
+
+        // Execute current tokens, now with redirection
+        execute(length);
+    }
+    else if (strcmp(type, "<") == 0) {
+        
+        // Open file
+        int file_desc = open(path, O_CREAT | O_RDWR);
+        
+        // –––––––––––––––––––– Error handling ––––––––––––––––––––
+        if (file_desc < 0) {
+            printf("Error opening the file\n"); 
+        }
+        if (errno != 0) {
+            perror("open");
+            printf("Error code %i.", errno);
+            errno = 0;
+        }
+
+        // Redirection of input from file
+        dup2(file_desc, 0);
+        close(file_desc);
+
+        // –––––––––––––––––––– Error handling ––––––––––––––––––––
+        if (errno != 0) {
+            perror("dup");
+            printf("Error code %i.", errno);
+            errno = 0;
+        }
+
+        // Execute current tokens, now with redirection
+        execute(length);
+    }
+    else if (strcmp(type, ">>") == 0) {
+        // TODO: Handle appending to file
+        
+    }
+}
+
+// Splits input into tokens, stores it in the global variable tokens
+// and returns the index of last token + 1.
+int tokenize() {
+    int i = 0;
+    
+    // Reset redirection flag
+    redirection = false;
+    
+    // Get first token
+    char *token = strtok(input, " ");
+
+    // Get the remaining tokens if any
+    while (token != NULL) {
+
+        // Check if there is a redirection
+        if (strcmp(token, "<") == 0 || strcmp(token, ">") == 0 || strcmp(token, ">>") == 0) {
+            redirection = true;
+        }
+        
+        tokens[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    
+    // This is normally the index of tokens' first NULL,
+    // but if tokens was entirely filled, i will be
+    // equal to the size of tokens (50, that is).
+    return i;
+}
+
+// Prompt the user for input and store it in the global variable input.
+void scanInput() {
+    // Get input
+    printf("$ ");
+    fgets(input, sizeof(input), stdin);
+    
+    // Remove newline character at end of last argument
+    int length = strlen(input);
+    input[length-1] = '\0';
+}
+
 // The shell loop
 void loop() {
     while (1) {
         // First get input
         scanInput();
         
+
+
+        
         // Tokenize
         int i = tokenize();
         
+        pid_t childID = fork();
+        if (errno != 0) {
+			perror("fork");
+			exit(errno);
+		}
+        if (childID == 0) {
+            if (redirection) {
+                io(tokens[i-2], tokens[i-1], i);
+            }
+            else {
+                execute(i);
+            }
+            exit(0);
+        }
         // Print tokens
         int n = 0;
         while (n < i) {
@@ -173,33 +181,18 @@ void loop() {
         }
 
         // Fork
-        pid_t childID = fork();
-        if (errno != 0) {
-			perror("fork");
-			exit(errno);
-		}
         
         // Execute if we're a child process
-        if (childID == 0) {
-            execute(i);
-            // execute(i);
-            exit(0);
-        }
         
         // Wait for child process to finish, then continue
-        pid_t zombie = waitpid(-1, NULL, WNOHANG);
-		if (errno != 0) {
-			perror("waitpid");
-			exit(errno);
-		}
-	
-		while (zombie > 0) {
+        pid_t zombie;
+		do {
 			zombie = waitpid(-1, NULL, WNOHANG);
 			if (errno != 0) {
 				perror("waitpid");
 				exit(errno);
 			}
-		}
+		} while (zombie > 0);
     }
 }
 
@@ -213,6 +206,5 @@ int main(int argc, char **argv) {
     loop();
     
     return 0;
-    
     
 }
